64f3025c70214fec51315328adfcb7ee60fb356a
Aritz Martínez <115811845+aritzmmartinez@users.noreply.github.com>
2024-05-30 22:15:09 +0200

    Started Tic Tac Toe

---
+++ projects/01 - Tic Tac Toe/src/App.jsx
@@ -0,0 +1,16 @@
+const TURNS = {
+    X: 'X',
+    O: 'O',
+}
+
+const board = Array(9).fill(null)
+
+function App() {
+    return (
+        <main className="board">
+            <h1>Tic Tac Toe</h1>
+        </main>
+    )
+}
+
+export default App
