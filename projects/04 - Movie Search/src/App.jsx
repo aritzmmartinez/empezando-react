1328a41f43c0870c64c2e50509435cd7d0b7f2ca
aritzmmartinez <aritz922@gmail.com>
2024-06-14 21:06:26 +0200

    Movie Search Finished

--- projects/04 - Movie Search/src/App.jsx
+++ projects/04 - Movie Search/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 import { useSearch } from './hooks/useSearch'
+import debounce from 'just-debounce-it'
 
 /*
     useRef crea una referencia mutable que persiste durante todo el ciclo de vida del componente
@@ -9,11 +10,18 @@ import { useSearch } from './hooks/useSearch'
 
     Y cada vez que cambia, no renderiza el componente. (A diferencia de useState que si que lo hace)
 */
-import { useRef } from 'react'
+import { useCallback, useRef, useState } from 'react'
 
 function App() {
+    const [sort, setSort] = useState(false)
     const { query, setQuery, error } = useSearch()
-    const { movies, getMovies, loading } = useMovies({ query })
+    const { movies, getMovies, loading } = useMovies({ query, sort})
+
+
+
+    const handleSort = () => {
+        setSort(!sort)
+    }
 
     const counter = useRef(0) // valor que persiste entre renders
     counter.current++
@@ -23,12 +31,26 @@ function App() {
         event.preventDefault()
         // aqui se ve el valor del input
         console.log({ query })
-        getMovies()
+        getMovies({ query })
     }
+
+    /*
+        Para que no se ejecute en cada renderizado, se puede usar useCallback
+    */
+    const debouncedGetMovies = useCallback(
+        debounce(query => {
+            getMovies({ query })
+        }, 300)
+    , [getMovies])
+
     const handleChange = (event) => {
         // esto seria una prevalidacion (impide que se introduzcan espacios al principio del input)
         const newQuery = event.target.value
         if (newQuery.startsWith(' ')) return
+
+        // de esta forma cada vez que se cambia el input, se actualiza el estado del query
+        // cuando el usuario deje de escribir durante 300 milisegundos, se ejecutara la funcion debouncedGetMovies
+        debouncedGetMovies(newQuery)
 
         // el target es el input, el value es el valor del input y el event es el evento (onChange)
         setQuery(event.target.value)
@@ -47,6 +69,7 @@ function App() {
                         Cada vez que el input cambia, se actualiza el estado del mismo
                     */}
                     <input onChange={handleChange} value={query} name='query' type='text' placeholder='Search for a movie...' />
+                    <input type='checkbox' onChange={handleSort} checked={sort} />
                     <button type='submit'>Search</button>
                 </form>
                 {error && <p style={{ color: 'red'}}>{error}</p>}
--- projects/04 - Movie Search/src/hooks/useMovies.js
+++ projects/04 - Movie Search/src/hooks/useMovies.js
@@ -1,7 +1,15 @@
-import { useState, useRef } from 'react'
+/*
+    El hook useMemo se utiliza para memorizar valores, en este caso se esta memorizando el valor de movies,
+    para que no se vuelva a calcular cada vez que se renderiza el componente dependiendo de una lista de dependencias, 
+    en este caso solo se calcula cuando query cambia
+
+    El hook useCallback es lo mismo que useMemo pero pensado para funciones, lo unico que este te permite simplificar el codigo
+*/
+
+import { useState, useRef, useMemo, useCallback } from 'react'
 import { searchMovies } from '../services/Movies'
 
-export function useMovies({ query }) {
+export function useMovies({ query, sort }) {
     /*
         Se puede dejar esto mas limpio moviendo este codigo a 
         una funcion o dejarlo asi:
@@ -38,24 +46,79 @@ export function useMovies({ query }) {
     // de esta forma se guarda el valor de query en la primera renderizacion
     const previousQuery = useRef(query)
     
-    const getMovies = async () => {
-        // si la query es la misma que la anterior, no se hace nada
-        if (query === previousQuery.current) return
+    /*
+        USANDO USEMEMO:
 
-        try {
-            setLoading(true)
-            previousQuery.current = query
-            const newMovies = await searchMovies({ query })
-            setMovies(newMovies)
+        const getMovies = useMemo(() => {
+            return async ({ query }) => {
+                // si la query es la misma que la anterior, no se hace nada
+                if (query === previousQuery.current) return
 
-        } catch (e) {
-            setError(e.message)
+                try {
+                    setLoading(true)
+                    setError(null)
+                    previousQuery.current = query
+                    const newMovies = await searchMovies({ query })
+                    setMovies(newMovies)
         
-            // tanto en el try como en el catch se ejecuta el finally
-        } finally {
-            setLoading(false)
-        }
-    }
+                } catch (e) {
+                    setError(e.message)
+                
+                    // tanto en el try como en el catch se ejecuta el finally
+                } finally {
+                    setLoading(false)
+                }
+            }
+            // en vez de pasarselo como depencia se le puede pasar por parametro
+        }, [])
+    */
 
-    return { movies, getMovies, loading}
+    /*
+        USANDO USECALLBACK: 
+    */
+    const getMovies = useCallback(
+        async ({ query }) => {
+            // si la query es la misma que la anterior, no se hace nada
+            if (query === previousQuery.current) return
+
+            try {
+                setLoading(true)
+                setError(null)
+                previousQuery.current = query
+                const newMovies = await searchMovies({ query })
+                setMovies(newMovies)
+    
+            } catch (e) {
+                setError(e.message)
+            
+                // tanto en el try como en el catch se ejecuta el finally
+            } finally {
+                setLoading(false)
+            }
+        // en vez de pasarselo como depencia se le puede pasar por parametro
+        },
+    [])
+
+
+  
+    // el localeCompare compara con aceptacion de tildes y mayusculas ya que si no, no ordenaria bien, por que la posicion de las letras en unicode no es la misma
+    /*
+        const sortedMovies = sort ? [...movies].sort((a, b) => a.title.localeCompare(b.title)) 
+        : movies
+    */
+
+    /*
+        se usa useMemo ya que si se hace como en el ejemplo de arriba se estaria calculando el valor de sortedMovies en cada renderizacion
+        ya que si no, lo que pasaria es que cada vez que cambiar la query (el valor del input) se volveria a calcular el valor de sortedMovies
+
+        asegurarse de que hay un problema de rendimiento antes de usar useMemo !!!
+    */
+    const sortedMovies = useMemo(() => {
+        return sort 
+        ? [...movies].sort((a, b) => a.title.localeCompare(b.title)) : movies
+
+        // cuando cambie el sort o los movies, se vuelve a calcular el valor de sortedMovies
+    }, [movies, sort])
+
+    return { movies : sortedMovies, getMovies, loading }
 }
